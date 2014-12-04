365d62d050fb34b47c6575d600a7e985ec5e9008
Vexal <porkchopsnbutter@gmail.com>
2014-12-03 20:44:55 -0600

    oops.

--- kernel/socket.cc
+++ kernel/socket.cc
@@ -1,6 +1,7 @@
 #include "socket.h"
 
 Socket::Socket(PacketProtocol protocol, int port, int owningProcess) :
+	Resource(ResourceType::SOCKET),
 	protocol(protocol),
 	port(port),
 	owningProcess(owningProcess)
